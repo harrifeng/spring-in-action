a73780d06634c374aba57ad9f310cc866242814d
Haoran Feng <haoran.feng@sap.com>
2015-04-14 18:33:59 -0700

    before use idea

---
+++ src/main/java/org/hfeng/qs/Action.java
@@ -0,0 +1,5 @@
+package org.hfeng.qs;
+
+public interface Action {
+    public String execute(String str);
+}
---
+++ src/test/java/org/hfeng/qs/ActionTest.java
@@ -0,0 +1,9 @@
+package org.hfeng.tmp;
+
+public class ActionTest {
+    public void testQuickStart() {
+        System.out.println("=======================");
+        System.out.println("Action Test Start!");
+        System.out.println("=======================");
+    }
+}
