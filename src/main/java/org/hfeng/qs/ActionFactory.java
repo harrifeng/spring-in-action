c878b5def63135d75aa4cf202a15eb4ba2809dfb
harri feng <harrifeng@gmail.com>
2015-04-15 21:38:50 +0800

    to make idea can use the gradle better, add plugin 'idea', ==> gradle idea can solve many problems

--- src/main/java/org/hfeng/qs/ActionFactory.java
+++ src/main/java/org/hfeng/qs/ActionFactory.java
@@ -1,5 +1,7 @@
 package org.hfeng.qs;
 
+import org.apache.commons.beanutils.BeanUtils;
+
 import java.io.FileInputStream;
 import java.util.Properties;
 
@@ -13,9 +15,10 @@ public class ActionFactory {
             String actionMessage = (String)pro.get(actionName + "_msg");
 
             Object obj = Class.forName(actionImplName).newInstance();
-
+            BeanUtils.setProperty(obj, "message", actionMessage);
+            return (Action)obj;
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
         return null;
     }
--- src/test/java/org/hfeng/qs/ActionTest.java
+++ src/test/java/org/hfeng/qs/ActionTest.java
@@ -7,9 +7,16 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class ActionTest {
     @Test
     public void testQuickStart() {
-
+        System.out.println("In testQuickStart--------->");
         ApplicationContext ctx = new FileSystemXmlApplicationContext("bean.xml");
         Action action = (Action)ctx.getBean("TheAction");
+        System.out.println(action.execute("Rod Johnson"));
+    }
+
+    @Test
+    public void testFactory() {
+        System.out.println("In testFactory----------->");
+        Action action = ActionFactory.getAction("TheAction");
         System.out.println(action.execute("Rod Johnson"));
     }
 }
